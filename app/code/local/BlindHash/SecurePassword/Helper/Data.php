dd329e1a6b89cd674670c6990650e0c655a5f576
Kana <icontractor@atlassoftweb.com>
2017-10-03 19:55:49 +0530

    Rename Extension

---
+++ app/code/local/BlindHash/SecurePassword/Helper/Data.php
@@ -0,0 +1,6 @@
+<?php
+
+class BlindHash_SecurePassword_Helper_Data extends Mage_Core_Helper_Abstract
+{
+    
+}
---
+++ app/code/local/BlindHash/SecurePassword/Model/Encryption/Interface.php
@@ -0,0 +1,26 @@
+<?php
+
+interface BlindHash_SecurePassword_Model_Encryption_Interface
+{
+
+    /**
+     * validate the password against the hash
+     *
+     * @param $password password
+     * @param $hash hash to validate against
+     *
+     * @return boolean
+     */
+    public function validateHash($password, $hash);
+
+    /**
+     * get the password hashed with the passed salt
+     *
+     * @param  string $password the users password
+     * @param bool|string $salt if false, then a salt will be
+     * generated, if string, the string will be used
+     *
+     * @return string the hashed password with the salt
+     */
+    public function getHash($password, $salt = false);
+}
