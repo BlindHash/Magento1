996299bafb46df0c450f0bb66e28febe29734091
Kana <icontractor@atlassoftweb.com>
2017-11-15 17:30:24 +0530

    Make Serverlist field readonly.

---
+++ app/code/community/BlindHash/SecurePassword/Block/Adminhtml/System/Config/Taplink/Serverlist.php
@@ -0,0 +1,11 @@
+<?php
+
+class BlindHash_SecurePassword_Block_Adminhtml_System_Config_Taplink_Serverlist extends Mage_Adminhtml_Block_System_Config_Form_Field
+{
+
+    protected function _getElementHtml(Varien_Data_Form_Element_Abstract $element)
+    {
+        $element->setDisabled('disabled');
+        return parent::_getElementHtml($element);
+    }
+}
